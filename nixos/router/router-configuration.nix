0f26045f4ced216481bef4f620bbd0d81e13bb0f
ArthurDelbarre <arthur.delbarre@epitech.eu>
2023-07-22 16:12:21 +0200

    [FIX] : network v3

--- nixos/router/router-configuration.nix
+++ nixos/router/router-configuration.nix
@@ -3,15 +3,21 @@ let
     # Network interface names for the external (internet-facing) and internal (local network) interfaces
     externalInterface = "enp0s1";  # Change this to match your external interface name
     internalInterface = "enp0s2";  # Change this to match your internal interface name
+    publicDnsServer = "8.8.8.8";
 in
 {
+    boot.kernel.sysctl = {
+        "net.ipv4.conf.all.forwarding" = true;
+    };
+
     # Define the networking options for the internal (local network) interface
     networking = {
         hostName = "router";
+        nameservers = [ "${publicDnsServer}" ];
 
-        # networkmanager = {
-        #     enable = true;
-        # };
+        networkmanager = {
+            enable = true;
+        };
 
         interfaces = {
             "${externalInterface}" = {
@@ -38,21 +44,19 @@ in
 
     };
 
-    boot.kernel.sysctl = {
-        "net.ipv4.conf.all.forwarding" = true;
-    };
-
     services = {
         # Define the DHCP server options for the internal (local network) interface
         dhcpd4 = {
             enable = true;
-            interfaces = [ internalInterface ];
+            interfaces = [ "${internalInterface}" ];
             extraConfig = ''
-                interface "${internalInterface}";
+                subnet 192.168.0 netmask 255.255.255.0 {
                 option routers 192.168.1.1;
-                option domain-name "local-network";
+                option domain-name-servers ${publicDnsServer};
                 option subnet-mask 255.255.255.0;
+                interface ${internalInterface};
                 range 192.168.1.100 192.168.1.200;
+                }
             '';
         };
     };
